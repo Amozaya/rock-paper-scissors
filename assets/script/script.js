9e8879df7c521ae59efaadd5a935f6165dc122dd
Oleg Fursa <olegfursa93@hotmail.com>
2023-06-27 13:39:03 +0000

    Add a list of possible game outcomes for Win, Draw, and Loose

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,6 +12,8 @@ const computerScore = 0;
 // Loops through each image in the options
 optionImages.forEach ((image, index) => {
     image.addEventListener('click', (e) => {
+      
+        // Add selected image to class active to change the opacity back to 1 when clicked
         image.classList.add('active');
 
         // Remove active when clicking on a different option
@@ -32,7 +34,60 @@ optionImages.forEach ((image, index) => {
        } else {
         userResult.src = "assets/images/spok.jpg";
        }
-       console.log(userSelection);
-          
+       
+
+       // Generate random number between 0 and 4
+       let randomNumber = Math.floor(Math.random() * 5);
+       console.log(randomNumber);
+
+       // Add variable to store src path to the images for computer guess
+       let computerImages = ["assets/images/rock.jpg", "assets/images/paper.jpg", "assets/images/scissors.jpg", "assets/images/lizzard.jpg", "assets/images/spok.jpg"]
+
+       // Replace computer result image with randomly generated one every time player makes a guess
+       computerResult.src = computerImages[randomNumber] 
+       
+       
+       // Assign a text value to the computer option (based on generated random number)(R -rock, P - paper, S - scissors, L - lizzard, SP - Spok)
+       let computerValue = ["R", "P", "S", "L", "SP"][randomNumber];
+
+       // Assign a text value to the player's option (based on index)
+       let playerValue = ["R", "P", "S", "L", "SP"][index];
+
+       // Variable to store all the possible outcomes of the game
+       let outcomes = {
+        RR: "Draw",
+        RP: "Loose",
+        RS: "Win",
+        RL: "Win",
+        RSP: "Loose",
+        PR: "Win",
+        PP: "Draw",
+        PS: "Loose",
+        PL: "Loose",
+        PSP: "Win",
+        SR: "Loose",
+        SP: "Win",
+        SS: "Draw",
+        SL: "Win",
+        SSP: "Loose",
+        LR: "Loose",
+        LP: "Win",
+        LS: "Loose",
+        LL: "Draw",
+        LSP: "Win",
+        SPR: "Win",
+        SPP: "Loose",
+        SPS: "Win",
+        SPL: "Loose",
+        SPSP: "Draw"
+       }
+
+       // Check the value based on player selection and computer generated selection
+       let outcomeValues = outcomes[playerValue + computerValue];
+
+       console.log(outcomeValues);
+
     });
 });
+
+
